4b316a1c07e93a6059fe07a6d0f5058ae0a75849
haidangdevhaui <haidangdevhaui@gmail.com>
2017-03-03 16:44:36 +0700

    u

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ var express = require('express'),
 
 app.use(express.static(__dirname + '/dist'));
 
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+app.use(forceSSL());
+
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, './dist/', 'index.html'));
 })
