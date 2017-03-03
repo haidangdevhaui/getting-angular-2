bf926590c431cbd1cd43c98bf68393b25c5e35a1
haidangdevhaui <haidangdevhaui@gmail.com>
2017-03-03 16:39:50 +0700

    u

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express'),
     path = require('path'),
     app = express();
 
+app.use(express.static(__dirname + '/dist'));
+
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, './dist/', 'index.html'));
 })
